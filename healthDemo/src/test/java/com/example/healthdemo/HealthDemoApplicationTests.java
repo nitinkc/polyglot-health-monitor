b1a8b6292d169d96728590ae04e22b687eacc55f
Nitin Chaurasia <gs.nitin@gmail.com>
2026-07-19 00:23:06 -0700

    demo application added

---
+++ healthDemo/src/test/java/com/example/healthdemo/HealthDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.healthdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthDemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
